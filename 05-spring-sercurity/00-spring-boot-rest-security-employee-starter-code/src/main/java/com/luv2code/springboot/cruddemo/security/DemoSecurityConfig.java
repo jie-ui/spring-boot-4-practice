b31484892b69fcf01c82fd34c0b9157c7d0588a9
jie-ui <aliceyangkk@gmail.com>
2026-03-11 13:12:59 -0400

    spring sercurity

---
+++ 05-spring-sercurity/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,76 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+@Bean
+//add support for jdbc nomore hardcode user
+public UserDetailsService userDetailsManager(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
+}
+
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configure->
+                configure
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        //user HTTP basic authentication
+    http.httpBasic(Customizer.withDefaults());
+    /*
+    无状态 REST API 使用 token 认证时，可以 禁用 CSRF 保护。
+
+    传统的 Web 应用 使用 session 认证时，通常需要 启用 CSRF 保护
+     */
+
+    //disaboe coress sit request forger(CSRF)
+    //in general , not required for statless rest APTs that user post . put. delete and /pr Patch
+    http.csrf(crsf -> crsf.disable());
+
+    return http.build();
+
+
+}
+
+
+   /* @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER","ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    */
+}
